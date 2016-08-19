37f2c0384344bbb9d4c83c9352559865c5961886
杨晓晨 <xiaochenyung@gmail.com>
2016-08-19 11:20:12 +0800

    init

---
+++ YCFoundationTests/YCFoundationTests.swift
@@ -0,0 +1,36 @@
+//
+//  YCFoundationTests.swift
+//  YCFoundationTests
+//
+//  Created by tm on 16/8/19.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+import XCTest
+@testable import YCFoundation
+
+class YCFoundationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
