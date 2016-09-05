23b5166f54d2db17ac688e251d095de439b486b6
杨晓晨 <xiaochenyung@gmail.com>
2016-09-05 15:35:04 +0800

    base class

---
+++ YCFoundation/YCFoundation/YCBaseFoundation.swift
@@ -0,0 +1,13 @@
+//
+//  YCBaseFoundation.swift
+//  YCFoundation
+//
+//  Created by tm on 16/9/5.
+//  Copyright © 2016年 tm. All rights reserved.
+//
+
+import UIKit
+
+class YCBaseFoundation: NSObject {
+
+}
